fee66b6c190cd99bfbeb451605fe7c6dcd236539
James S <wolfbitez23@gmail.com>
2018-05-21 09:50:48 -0400

    Create cleverbot.py

---
+++ cleverbot.py
@@ -0,0 +1,39 @@
+import json
+try:
+    import requests
+except ModuleNotFoundError:
+    print("Package \"requests\" not found, auto-installing..")
+    import os
+    os.system('py -m pip install requests')
+    import requests
+
+
+class Client(object):
+    def __init__(self, user, key, nick):
+        self.user = user
+        self.key = key
+        self.nick = nick
+
+        body = {
+            'user': user,
+            'key': key,
+            'nick': nick
+        }
+
+        requests.post('https://cleverbot.io/1.0/create', json=body)
+
+    def ask(self, text):
+        body = {
+            'user': self.user,
+            'key': self.key,
+            'nick': self.nick,
+            'text': text
+        }
+
+        r = requests.post('https://cleverbot.io/1.0/ask', json=body)
+        r = json.loads(r.text)
+
+        if r['status'] == 'success':
+            return r['response']
+        else:
+            return False
