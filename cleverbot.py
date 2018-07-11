fbbc865d576cd85a6c90bb950b920ceb1cbd91ef
James S <wolfbitez23@gmail.com>
2018-07-10 23:08:59 -0400

    Implemented async_ask

--- cleverbot.py
+++ cleverbot.py
@@ -1,12 +1,5 @@
-import json
-try:
-    import requests
-except ModuleNotFoundError:
-    print("Package \"requests\" not found, auto-installing..")
-    import os
-    os.system('py -m pip install requests')
-    import requests
-
+import json, asyncio, requests
+from aiohttp import ClientSession
 
 class Client(object):
     def __init__(self, user, key, nick):
@@ -20,6 +13,7 @@ class Client(object):
             'nick': nick
         }
 
+        
         requests.post('https://cleverbot.io/1.0/create', json=body)
 
     def ask(self, text):
@@ -37,3 +31,22 @@ class Client(object):
             return r['response']
         else:
             return False
+
+    async def async_ask(self, text):
+        body = {
+            'user': self.user,
+            'key': self.key,
+            'nick': self.nick,
+            'text': text
+        }
+        
+        async with ClientSession() as session:
+            async with session.post('https://cleverbot.io/1.0/ask', json=body) as response:
+
+                response = await response.read()
+                r = json.loads(response)
+
+                if r['status'] == 'success':
+                    return r['response']
+                else:
+                    return False
